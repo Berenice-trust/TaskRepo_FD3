c958ea28023b4b3cc8b12660fb0ba8d91168c152
Veranika <98880112+Berenice-trust@users.noreply.github.com>
2025-07-10 02:30:54 +0300

    Start Filter

---
+++ HW_02/Filter/components/Filter.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types"; // импортируем PropTypes для валидации
+import "./Filter.scss";
+
+// классовый коспонент, расширяет React.Component
+class Filter extends React.Component {
+  // свойство
+  state = {
+   
+  };
+
+
+
+  render() {
+    return 
+     
+  }
+}
+
+// Типы props, тип и обязательность
+// это то, что получено от родителя App.js
+Filter.propTypes = {
+  
+};
+
+export default Filter;
