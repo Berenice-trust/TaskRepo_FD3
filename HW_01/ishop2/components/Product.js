da21366e30ef35e95a0d64131ab3eb08af327e79
Veranika <98880112+Berenice-trust@users.noreply.github.com>
2025-07-10 10:56:09 +0300

    Product to class

--- HW_01/ishop2/components/Product.js
+++ HW_01/ishop2/components/Product.js
@@ -1,41 +1,57 @@
 import React from "react"; //  по идее эта строчка и не нужна
 import PropTypes from "prop-types";
 
-function Product({
-  name,
-  price,
-  photoUrl,
-  count,
-  id,
-  selectedProductId,
-  cbSelectProduct,
-  cbDeleteProduct,
-}) {
-  return (
-    <tr
-      className={selectedProductId === id ? "selected" : ""}
-      onClick={() => cbSelectProduct(id, name)} // вызывается при клике на строку
-    >
-      <td>{name}</td>
-      <td>{price} руб.</td>
-      <td>
-        <img src={photoUrl} alt={name} />
-      </td>
-      <td>{count}</td>
-      <td>
-        <button
-          type="button"
-          onClick={(e) => {
-            e.stopPropagation(); // отмена всплытия клика, чтобы не выделялась строка
-            cbDeleteProduct(id, name); // callback удаления товара
-          }}
-        >
-          Удалить
-        </button>
-      </td>
-    </tr>
-  );
-}
+class Product extends React.Component{
+
+  handleSelectProduct = () => {
+    // деструктуризация props
+    const { id, name, cbSelectProduct } = this.props;
+    cbSelectProduct(id, name); // вызывается при клике на строку
+  };
+
+  handleDeleteProduct = (e) => {
+    // деструктуризация props
+    const { id, name, cbDeleteProduct } = this.props;
+    e.stopPropagation(); // отмена всплытия клика, чтобы не выделялась строка
+    cbDeleteProduct(id, name); // вызывается при клике на строку
+  };
+
+
+  render() {
+    const {
+      name,
+      price,
+      photoUrl,
+      count,
+      id,
+      selectedProductId,
+    } = this.props;
+
+    return (
+      <tr
+        className={selectedProductId === id ? "selected" : ""}
+        onClick={this.handleSelectProduct} // вызывается при клике на строку
+      >
+        <td>{name}</td>
+        <td>{price} руб.</td>
+        <td>
+          <img src={photoUrl} alt={name} />
+        </td>
+        <td>{count}</td>
+        <td>
+          <button
+            type="button"
+            onClick={this.handleDeleteProduct} // callback удаления товара
+          >
+            Удалить
+          </button>
+        </td>
+      </tr>
+    );
+  }
+
+
+};
 
 Product.propTypes = {
   id: PropTypes.number.isRequired,
